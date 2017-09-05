450dbd3eae87f776bc6e9fa19a9af87b10f4c1b7
Forrest Collman <forrest.collman@gmail.com>
2017-09-05 16:14:26 -0700

    changing name of default repo

--- diced_startup.py
+++ diced_startup.py
@@ -1,6 +1,6 @@
 from diced import DicedStore, ArrayDtype
 import time
-store = DicedStore("/data/synbio.diced") # location of diced repo
+store = DicedStore("/data/repo.diced") # location of diced repo
 #repo = store.create_repo("firstrepo", "My first repo")
 while True:
     time.sleep(.5)
